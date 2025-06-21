b85728046752ff8791c1d8394348e8f87960c9b6
Mahmoud Ismail <108597788+mahmoudismail2000@users.noreply.github.com>
2025-06-21 21:38:05 +0300

    fleet card routing

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -4,12 +4,13 @@ import { BrandComponent } from '../brand/brand.component';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { CarItemComponent } from '../car-item/car-item.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
-  imports:[CommonModule,IonicModule,FleetOptionComponent,BrandComponent,CarItemComponent],
+  imports:[CommonModule,IonicModule,FleetOptionComponent,BrandComponent,CarItemComponent,RouterLink],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   standalone: true,
 })
